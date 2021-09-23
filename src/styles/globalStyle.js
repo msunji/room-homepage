f859edec994086cf39338e589d466cf85da69ce1
Marge <marge.consunji@gmail.com>
2021-09-23 13:16:21 +0800

    Added text styling for p and h3 elements

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -12,12 +12,36 @@ const GlobalStyle = createGlobalStyle`
     --sans-serif: "Spartan", sans-serif;
   }
 
+  * {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+
   body, html {
     overflow: hidden;
     height: 100vh;
     font-size: 12px;
     font-family: var(--sans-serif);
     font-weight: 500;
+  }
+
+  h2 {
+    color: var(--black);
+    line-height: 1.83;
+    font-weight: 700;
+    letter-spacing: 5px;
+    text-transform: uppercase;
+  }
+  p {
+    color: var(--grey-dark);
+    line-height: 1.6;
+    letter-spacing: -0.25px;
   }
 `;
 
