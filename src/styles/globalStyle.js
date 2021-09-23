df291f1d1baf2574e7b9772d9cf3dc3af4766875
Marge <marge.consunji@gmail.com>
2021-09-23 12:58:12 +0800

    Set CSS reset, set body font size

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -1,11 +1,23 @@
+import reset from "styled-reset";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
   :root {
     --grey-dark: hsl(0, 0%, 63%);
     --grey-very-dark: hsl(0, 0%, 27%);
     --white: hsl(0, 0%, 100%);
     --black: hsl(0, 0%, 0%);
+    --sans-serif: "Spartan", sans-serif;
+  }
+
+  body, html {
+    overflow: hidden;
+    height: 100vh;
+    font-size: 12px;
+    font-family: var(--sans-serif);
+    font-weight: 500;
   }
 `;
 
