d9d999c8231b58c94dae7bdc44d90ace799dccb4
Marge <marge.consunji@gmail.com>
2021-09-23 18:44:05 +0800

    Made main grid 100vh

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { About } from "./components/About";
 const MainGrid = styled.main`
   display: grid;
   grid-template-columns: 1fr;
+  height: 100vh;
 
   @media screen and ${breakpoints.lg} {
     grid-template-rows: 2fr 1fr;
