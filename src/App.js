6f9c439d0508ba00a65f106a633005abe24223b0
Marge <marge.consunji@gmail.com>
2021-09-23 17:21:04 +0800

    Added larger main grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import GlobalStyle from "../src/styles/globalStyle";
+import breakpoints from "./styles/breakpoints";
+import styled from "styled-components";
 import { Slides } from "./components/slides/Slides.js";
 import { About } from "./components/About";
+
+const MainGrid = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+
+  @media screen and ${breakpoints.lg} {
+    grid-template-rows: 2fr 1fr;
+  }
+`;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <main>
+      <MainGrid>
         <Slides />
         <About />
-      </main>
+      </MainGrid>
     </>
   );
 }
