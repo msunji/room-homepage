7449d6d2865c4a0fdfe086983dd7be88d5f8920e
Marge <marge.consunji@gmail.com>
2021-09-28 16:44:22 +0800

    Removed Nav component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import breakpoints from "./styles/breakpoints";
 import styled from "styled-components";
 import { Slides } from "./components/slides/Slides.js";
 import { About } from "./components/About";
-import { Nav } from "./components/nav/Nav";
 
 const MainGrid = styled.main`
   display: grid;
   grid-template-columns: 1fr;
   height: 100vh;
-  position: relative;
 
   @media screen and ${breakpoints.lg} {
-    grid-template-rows: 66.7% 1fr;
+    grid-template-rows: 66.7% auto;
   }
 `;
 
@@ -20,7 +18,6 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Nav />
       <MainGrid>
         <Slides />
         <About />
